1affdc4ef2e580d79e2532ebce473bef9be5f4bd
sai_chandu_3 <saichandu3397belide@gmail.com>
2022-07-13 15:38:25 +0530

    Ios download changed to NSURLSessionDownload for asychronous

--- src/ios/FileDownload.m
+++ src/ios/FileDownload.m
@@ -1,36 +1,49 @@
 #import "FileDownload.h"
 #import <Cordova/CDVPlugin.h>
 
+
+
 @implementation FileDownload
 
 - (void)downloadFile:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
 
-    @try{
-        NSDictionary* options = [command.arguments objectAtIndex:0];
+    NSDictionary* options = [command.arguments objectAtIndex:0];
 
-        NSString *uri   = options[@"uri"];
-        NSString *fileName = options[@"fileName"];
-
-        NSData *pdfData = [[NSData alloc] initWithContentsOfURL:[
-        NSURL URLWithString:uri]];
-
-        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString *documentsDirectory = [paths objectAtIndex:0];
-        NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileName];
-        [pdfData writeToFile:filePath atomically:YES];
-
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:filePath];
-
-    }
-    @catch(NSException *exception) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-
-    }
+    NSString *uri   = options[@"uri"];
+    NSURL *url = [NSURL URLWithString:uri];
+    NSString *fileName = options[@"fileName"];
     
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    __block NSURLSessionDownloadTask task = [session downloadTaskWithRequest:request completionHandler : ^(NSURL   responseUrl, NSURLResponse  response, NSError  error){
+        if (error != nil)
+        {
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            [task suspend];
+        }
+        else
+        {
+            
+            NSArray  *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+            NSString *documentsDirectory = [paths objectAtIndex:0];
+            NSString *filePath = [documentsDirectory stringByAppendingPathComponent:fileName];
+            NSData *fileData = [[NSData alloc] initWithContentsOfURL:responseUrl];
+            [fileData writeToFile:filePath atomically:YES];
+
+            
+            CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:filePath];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+            [task suspend];
+        }
+
+    }];
+    
+    [task resume];
+
 }
 
 @end
